1391aec3bffb3efac185f7d553d0012816a8d9b2
Mile Divovic <md2950@nyu.edu>
2020-02-14 14:59:12 -0800

    add apache license

--- js/epub-fetch/content_document_fetcher.js
+++ js/epub-fetch/content_document_fetcher.js
@@ -1,3 +1,17 @@
+/*
+Copyright 2020 Eduworks Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+Modified in commit 81f1e18bc58784d67bd4fa1302875f1ef4e38049
+*/
+
 //  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification,
